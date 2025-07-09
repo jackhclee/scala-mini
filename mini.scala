db9ddcf8b91900e2be298e10757cb28b9dc4ad34
Jack Lee <jack@uknow.com>
2025-07-09 23:33:50 +0800

    mini2

--- mini.scala
+++ mini.scala
@@ -1,9 +1,11 @@
 //> using scala 2.13
-//> using dep com.lihaoyi::requests:0.8.0
+//> using dep com.lihaoyi::requests:0.9.0
 
 
 object App {
   def main(args: Array[String]) = {
     println("Hello World")
+    val res = requests.get("https://httpbin.dev/uuid")
+    println(res.text)
 }
 }
